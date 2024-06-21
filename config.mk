1992d8dfdd9a2b7eed4323b597f8ec2d07a4f1ab
Philip East <philip.east@crick.ac.uk>
2024-06-21 15:01:28 +0100

    added venv path

--- config.mk
+++ config.mk
@@ -3,6 +3,7 @@ singularity_command=$(call ml,Singularity/3.6.4); singularity
 conda_command=$(call ml,Anaconda3/2023.09-0); conda
 CONDA_INSTALL_HOME=/camp/apps/eb/software/Anaconda3
 PYTHON_ENV_HOME=$(shell readlink -f ./env)
+PYTHON_VENV_HOME=$(shell readlink -f ./venv)
 rocker_image=bind
 R_version=4.3.1
 RENV_PATHS_ROOT=/camp/stp/babs/working/software/renv
